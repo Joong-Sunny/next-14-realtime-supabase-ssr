712b51e9e8f609238c7215aa3b5fea5123f3f64d
sunny <dance@kakao.com>
2024-01-26 20:35:57 +0900

    🚀 page protection

--- app/auth-server-action/components/RegisterForm.tsx
+++ app/auth-server-action/components/RegisterForm.tsx
@@ -17,14 +17,15 @@ import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/use-toast";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import {signUpWithEmailAndPassword} from "@/app/auth-server-action/actions";
 
 const FormSchema = z
 	.object({
 		email: z.string().email(),
-		password: z.string().min(6, {
+		password: z.string().min(3, {
 			message: "Password is required.",
 		}),
-		confirm: z.string().min(6, {
+		confirm: z.string().min(3, {
 			message: "Password is required.",
 		}),
 	})
@@ -42,17 +43,39 @@ export default function RegisterForm() {
 		},
 	});
 
-	function onSubmit(data: z.infer<typeof FormSchema>) {
-		toast({
-			title: "You submitted the following values:",
-			description: (
-				<pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+	async function onSubmit(data: z.infer<typeof FormSchema>) {
+
+		const result = await signUpWithEmailAndPassword(data)
+		const {error} = JSON.parse(result)
+
+
+		if(error?.message){
+			toast({
+				variant: "destructive",
+				title: "You submitted the following values:",
+				description: (
+					<pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
 					<code className="text-white">
-						{JSON.stringify(data, null, 2)}
+						{error.message}
+						{/*{JSON.stringify(data, null, 2)}*/}
 					</code>
 				</pre>
-			),
-		});
+				),
+			});
+
+		}else{
+			toast({
+				title: "You submitted the following values",
+				description: (
+					<pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+					<code className="text-white">
+						Successfully resgistered!
+						{/*{JSON.stringify(data, null, 2)}*/}
+					</code>
+				</pre>
+				),
+			});
+		}
 	}
 
 	return (
