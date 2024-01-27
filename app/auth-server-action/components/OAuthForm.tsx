76009fd8bb2ab972d0e2efe01f9985b0036595fa
sunny <dance@kakao.com>
2024-01-27 16:26:34 +0900

    🚀 Oauth(github) setting done( but it's not working, supabase not set yet)

--- app/auth-server-action/components/OAuthForm.tsx
+++ app/auth-server-action/components/OAuthForm.tsx
@@ -1,6 +1,27 @@
+'use client'
+
+import { createBrowserClient } from '@supabase/ssr'
+
+
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function OAuthForm() {
-	return <Button className="w-full">Login With Github</Button>;
+
+	const supabase = createBrowserClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+	)
+
+	const loginWithGithub = () => {
+		supabase.auth.signInWithOAuth({
+			provider: 'github',
+			options: 	{
+			redirectTo: `${Location.origin}/auth-server-action/callback`,
+		}
+	})
+	}
+
+
+	return <Button className="w-full" onClick={loginWithGithub}>Login With Github</Button>;
 }
