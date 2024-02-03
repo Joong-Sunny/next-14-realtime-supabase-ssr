edae93885f146fb33f6bdc6be2f99935b475eaa0
sunny <dance@kakao.com>
2024-02-04 00:41:20 +0900

    🚀DB에서  지속적으로 테이블 변화감지를 구독할수 있는 로직 구현

--- app/auth-server-action/components/OAuthForm.tsx
+++ app/auth-server-action/components/OAuthForm.tsx
@@ -4,13 +4,12 @@ import { createBrowserClient } from '@supabase/ssr'
 import { Button } from "@/components/ui/button";
 import React from "react";
 
+export const supabase = createBrowserClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL!,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
+
 export default function OAuthForm() {
-
-	const supabase = createBrowserClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-	)
-
 	const loginWithGithub = () => {
 		console.log(`${location.origin}/auth-server-action/callback`)
 
---
+++ app/todo/components/Todos.tsx
@@ -0,0 +1,85 @@
+"use client"
+
+import React, {useEffect, useState} from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {deleteTodoById, readTodo, updateTodoById} from "@/app/todo/actions";
+import {supabase} from "@/app/auth-server-action/components/OAuthForm";
+
+export default function Todos() {
+  const [todos, setTodos] = useState<unknown[]>([])
+
+  useEffect(() => {
+    fetchTodos()
+
+    const subscription = supabase.channel('table-db-changes').on('postgres_changes', {
+      event: '*',
+      schema: 'public',
+      table: 'crud-demo',
+    },(payload) => {
+      updateTodosBasedOnPayload(payload)
+    }).subscribe()
+
+    return () => {
+      supabase.removeChannel(subscription);
+    };
+  }, [])
+
+
+const fetchTodos = async () => {
+
+  console.log("저는 딱 한번만 호출되어야 하는 TODO fetch 입니다");
+  const {data:fetchedTodo} = await readTodo()
+  if (fetchedTodo)
+  setTodos(fetchedTodo)
+}
+  const updateTodosBasedOnPayload = (payload) => {
+    // payload를 분석하여 상태 업데이트 로직 구현
+    console.log("방금바뀐정보는....", payload)
+
+    if (payload.eventType === 'INSERT') {
+      setTodos((prevTodos) => [...prevTodos, payload.new]);
+    }
+
+    else if (payload.eventType === 'UPDATE') {
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === payload.new.id ? payload.new : todo))
+      );
+    }
+
+    else if (payload.eventType === 'DELETE') {
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== payload.old.id));
+    }
+  };
+
+
+  return (
+    <>
+        {todos?.map((todo, index) => {
+          const deleteTodo = deleteTodoById.bind(null, todo.id)
+          const updateTodo = updateTodoById.bind(null, todo.id, !todo.completed)
+
+          return (
+            <div key={index} className="flex items-center gap-6">
+              <h1
+                className={cn({
+                  "line-through": todo.completed,
+                })}
+              >
+                {todo.title}
+              </h1>
+
+              <form action={deleteTodo}>
+                <Button>delete</Button>
+              </form>
+
+              <form action={updateTodo}>
+                <Button>Update</Button>
+              </form>
+            </div>
+          );
+        })}
+    </>
+  );
+}
+
